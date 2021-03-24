d5ce40d8809711b354a3c6180331e7a5c0cad540
linlif <1264954361@qq.com>
2021-03-24 15:13:56 +0800

    bug fix

--- src/routes/api/blog-home.js
+++ src/routes/api/blog-home.js
@@ -9,8 +9,12 @@ const { genValidator } = require('../../middlewares/validator')
 const { create } = require('../../controllers/blog-home')
 
 router.post('/create', genValidator(blogValidate), async (ctx, next) => {
-    const { content, image } = ctx.request.body,
-        { id: userId } = ctx.state.user;
+    const { content, image } = ctx.request.body;
+    // jwt获取用户信息
+    // const { id: userId } = ctx.state.user;
+
+    // koa-session2 + ioredis 获取用户信息的方式
+    const { id: userId } = ctx.session.userInfo;
 
     ctx.body = await create({
         userId,
